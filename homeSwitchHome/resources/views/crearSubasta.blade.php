bf03f827c0742bc76e6357d78a71b3803572cf17
Rodrigo Jara <rodrigojara1997@gmail.com>
2019-05-12 20:57:26 -0300

    Terminado backend Crear subasta y ver detalle subasta

--- homeSwitchHome/resources/views/crearSubasta.blade.php
+++ homeSwitchHome/resources/views/crearSubasta.blade.php
@@ -8,24 +8,29 @@
 
 		<div id="container">
 			<div id="main">
+				<p>Podes acceder a las variables de fecha de inicio libre del hospedaje usando <?php
+						echo '{{'.'$fechaInicioHospedaje'.'}}';
+					?> y <?php
+						echo '{{'.'$fechaFinHospedaje'.'}}';
+					?> </p>
+
 
 			<div  id="cent" class="text-center">
-				<h1>Crear nueva subasta para el hospedaje TITULO</h1>
+				<h1>Crear nueva subasta para el hospedaje {{$titulo}}</h1>
 				<div id="centrado" class="col-md-8 text-center border rounded shadow-sm">
-			<form action="crearsubasta/validar" method="post">
+			<form action="/crearsubasta/validar" method="post">
 				{{ csrf_field() }}
 			    <div class="form-group">
 			      	<label for="montoBase">Monto base</label>
-		     		 <input type="text" class="@error('montoBase') is-invalid @enderror form-control form-control-sm" name="montoBase" id="montoBase" placeholder="Monto base de la subasta">
+		     		 <input type="text" class="form-control form-control-sm" name="montoBase" id="montoBase" placeholder="Monto base de la subasta">
 			    </div>
 			  	<div class="form-group">
 			      	<label for="fechaInicio">Fecha inicial</label>
 		      		<input type="date" class="form-control form-control-sm" name="fechaInicio" id="fechaInicio">
 				</div>
-			  	<div class="form-group">
-			      	<label for="fechaFin">Fecha inicial</label>
-	      			<input type="date" class="form-control form-control-sm" name="fechaFin" id="fechaFin">
-			  	</div>
+				<input type="hidden" name="fechaInicioHospedaje" value="{{$fechaInicioHospedaje}}">
+				<input type="hidden" name="fechaFinHospedaje" value="{{$fechaFinHospedaje}}">
+				<input type="hidden" name="idHospedaje" value="{{$idHospedaje}}">
 			  	<div class="form-group">
 			  		<button type="submit" class="btn btn-primary">Crear subasta</button>
 			  		<a class="btn btn-secondary my-2 my-sm-0" href="{{ url('/') }}">Cancelar</a>
