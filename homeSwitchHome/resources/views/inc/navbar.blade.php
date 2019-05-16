a359827ecaa1c0c606e2cfc4972cd993e5f0a52d
Cesar-Morales <moraleescesar@gmail.com>
2019-05-15 23:39:55 -0300

    arreglo de img

--- homeSwitchHome/resources/views/inc/navbar.blade.php
+++ homeSwitchHome/resources/views/inc/navbar.blade.php
@@ -6,7 +6,7 @@
 </style>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><img src="/images/{{ $nombreImagen }}" width="35" height="25" class="d-inline-block align-top">
+  <a class="navbar-brand" href="#"><img src="/images/Logo.png" width="35" height="25" class="d-inline-block align-top">
    homeSwitchHome</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
