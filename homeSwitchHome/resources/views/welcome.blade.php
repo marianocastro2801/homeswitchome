7add3184a7a8f690f25cdf9e4eb89ce9c9f05e48
Cesar-Morales <moraleescesar@gmail.com>
2019-05-16 08:17:22 -0300

    add if

--- homeSwitchHome/resources/views/welcome.blade.php
+++ homeSwitchHome/resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.baseapp')
-
-
+	<div>
+        @if(session('exito'))
+            <div class="alert alert-success">
+                {{ session('exito') }}
+            </div>
+        @endif
+    </div>
 @section('content')
 @endsection
