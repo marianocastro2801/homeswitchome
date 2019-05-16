5f1a9a7e6b6ba745af1e875ae55dfeba328234b2
Cesar-Morales <moraleescesar@gmail.com>
2019-05-15 22:49:25 -0300

    NavBar final. Footer final. Cargar detalle de subasta

--- homeSwitchHome/resources/views/inc/navbar.blade.php
+++ homeSwitchHome/resources/views/inc/navbar.blade.php
@@ -1,24 +1,43 @@
+<style type="text/css">
+  .itemnav{
+    margin-left: 15px;
+    margin-right: 15px;
+  }
+</style>
+
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">homeSwitchHome</a>
+  <a class="navbar-brand" href="#"><img src="/images/{{ $nombreImagen }}" width="35" height="25" class="d-inline-block align-top">
+   homeSwitchHome</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contacto</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Preguntas frecuentes</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Buscar..." aria-label="Search">
-      <button class="btn btn-primary my-2 my-sm-0" type="submit">Buscar</button>
-    </form>
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">    
+    <div >
+      <ul class="navbar-nav" >
+        <li class="nav-item">
+          <a class="nav-link itemnav" href="#">Localidades<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item itemnav">
+          <a class="nav-link" href="#">HotSale</a>
+        </li>
+        <li class="nav-item itemnav">
+          <a class="nav-link" href="#">Subastas</a>
+        </li>
+      </ul>
+    </div>
+      <div style="float:right">
+        <div class="btn-group">
+          <button type="button" class="btn btn-info dropdown-toggle" style="margin-top: 5px" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            NOMBRE-DEL-USUARIO
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#"><span class="glyphicon glyphicon-user" style="margin-right: 10px"></span>Mi Perfil</a>
+            <a class="dropdown-item" href="#"><span class="glyphicon glyphicon-info-sign" style="margin-right: 10px"></span>Contactos</a>
+            <a class="dropdown-item" href="#"><span class="glyphicon glyphicon-question-sign" style="margin-right: 10px"></span>FAQ</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#"><span class="glyphicon glyphicon-off" style="margin-right: 10px"></span>Cerrar sesion</a>
+          </div>
+      </div>
+      </div>
   </div>
 </nav>
--- homeSwitchHome/resources/views/layouts/baseapp.blade.php
+++ homeSwitchHome/resources/views/layouts/baseapp.blade.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html>
 	<head>
-    </style>
 		<title>homeswitchHome</title>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="canonical" href="https://getbootstrap.com/docs/3.4/examples/navbar/">
 		<link rel="shortcut icon" type="image/x-icon" href="images/Logo.png" / >
 		<link rel="stylesheet" href="/css/app.css">
-		
-		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		@include('inc.navbar')
 		@yield('content')
 		@include('inc.footer')
+		
 	</body>
 
 </html>
