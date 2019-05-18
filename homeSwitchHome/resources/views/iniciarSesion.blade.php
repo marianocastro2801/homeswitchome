e7ec0109bcddf6b591acfefa08a52db872e8ec6e
Cesar-Morales <moraleescesar@gmail.com>
2019-05-18 17:36:40 -0300

    /sesion con diseño

--- homeSwitchHome/resources/views/iniciarSesion.blade.php
+++ homeSwitchHome/resources/views/iniciarSesion.blade.php
@@ -1,5 +1,14 @@
-<form action="/sesion/iniciar" method="post">
-				{{ csrf_field() }}
-				<input type="text" name="nombreUsuario" placeholder="Ingrese su nombre">
-				<button type="submit">Iniciar</button>
-</form>
+@extends('layouts.baseapp')
+@section('content')
+	<div id="cent" class="text-center col-md-3 center">
+		<form class="form-signin" action="/sesion/iniciar" method="post">
+			<h1 class="h3 mb-3 font-weight-normal">Por favor ingrese el nombre de usuario</h1>
+			{{ csrf_field() }}
+			<input type="text" name="nombreUsuario" class="text-center form-control">
+			<br>
+			<button type="submit" class="btn btn-lg btn-primary btn-block">
+				Iniciar
+			</button>
+		</form>
+	</div>
+@endsection
