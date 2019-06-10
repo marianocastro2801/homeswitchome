67da7f17865659a5210c03822bc538167d574a9e
Cesar-Morales <moraleescesar@gmail.com>
2019-06-10 08:45:09 -0300

    registro e inicio de sesion

--- homeSwitchHome/resources/views/logIn.blade.php
+++ homeSwitchHome/resources/views/logIn.blade.php
@@ -1,20 +1,38 @@
 @extends('layouts.baseapp')
 @section('content')
-	<div id="cent" class="text-center col-md-3 center">
-		<form class="form-signin">
-	  		<img class=" mb-4" src="/images/Logo.png" alt="" width="90" height="72">
-	  		<h1 class="h3 mb-3 font-weight-normal">Por favor ingrese sus datos</h1>
-	  		<label for="inputEmail" class="sr-only">Direccion email</label>
-	  		<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-	  		<br>
-			<label for="inputPassword" class="sr-only">Contraseña</label>
-			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-			<div class="checkbox mb-3">
-			    <label>
-			      <input type="checkbox" value="remember-me"> Recordarme
-			    </label>
-			 </div>
-	  		<button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar sesion</button>
-		</form>
+<style type="text/css">
+	.fondo{
+		margin-top: 50; 
+		padding: 25px;  
+		border-radius:25px; 
+		background: black;
+	}
+</style>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<!--Futura Publicidad-->
+		</div>
+		<div class="fondo text-center col-md-4">
+			<form class="form-signin">
+		  		<img class=" mb-4" src="/images/Completo.png" alt="" width="330" height="150">
+		  		<hr>
+		  		<h3 class="font-weight-normal text-white">Iniciar Sesion</h3>
+		  		<hr>
+		  		<div class="form-group">
+			  		<label for="inputNombre" class="control-label text-white">Nombre de Usuario</label>
+			  		<input type="email" id="inputEmail" class="form-control" placeholder="Andres Perez" required autofocus>
+		  		</div>
+		  		<div class="form-group">
+					<label for="inputPassword" class="control-label text-white">Contraseña</label>
+					<input type="password" id="inputPassword" class="form-control" placeholder="********" required>
+				</div>
+		  		<button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar sesion</button>
+			</form>
+		</div>
+		<div class="col-md-4">
+			<!--Futura Publicidad-->
+		</div>
 	</div>
+</div>
 @endsection
---
+++ homeSwitchHome/resources/views/registrarUsuario.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.baseapp')
+@section('content')
+<style type="text/css">
+	.fondo{
+		margin-top: 50; 
+		padding: 25px;  
+		border-radius:25px; 
+		background: black;
+		margin-bottom: 60px;
+
+	}
+	.credit-card-div  span { padding-top:10px; }
+	.credit-card-div img { padding-top:30px; }
+	.credit-card-div .small-font { font-size:9px; }
+	.credit-card-div .pad-adjust { padding-top:10px; }
+</style>
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			<!--Futura Publicidad-->
+		</div>
+		<div class="fondo text-center col-md-6">
+			<form>
+				<img class=" mb-4" src="/images/Completo.png" alt="" width="330" height="150">
+		  		<hr>
+		  		<h1 class="h3 mb-3 font-weight-normal text-white">Registrarse</h1>
+		  		<hr>
+		  		<div class="form-group row"> <!-- Full Name -->
+		  			<div class="col-md-6">
+				        <label for="nombre" class="control-label text-white">Nombre</label>
+				        <input type="text" class="form-control" id="full_name_id" name="full_name" placeholder="Andres">
+			        </div>
+			        <div class="col-md-6">
+				        <label for="apellido" class="control-label text-white">Apellido</label>
+				        <input type="text" class="form-control" id="full_name_id" name="full_name" placeholder="Perez">
+			        </div>
+			    </div>
+			    <div class="form-group">
+			    	<label for="email" class="control-label text-white">Email</label>
+                    <input id="email" name="email" type="text" placeholder="Correo Electronio" class="form-control">
+                </div>
+              	<div class="form-group row">
+              		<div class="col-md-6">
+              			<label for="pwd" class="control-label text-white">Contraseña</label>
+              			<input type="password" class="form-control" id="pwd" placeholder="********">
+              		</div>
+              		<div class="col-md-6">
+              			<label for="pwd2" class="control-label text-white">Confirme contraseña</label>
+              			<input type="password" class="form-control" id="pwd2" placeholder="********">
+              		</div>
+              	</div>
+              	<div class="form-group">
+              		<label for="dni" class="control-label text-white">Documento Nacional de identidad</label>
+			        <input class="form-control" id="dni" name="dni" placeholder="99.129.880">
+              	</div>
+              	<div class="form-group">
+					<label for="fechaNacimiento" class="control-label text-white">Fecha de Nacimiento</label>
+					<input type="date" class="form-control form-control-sm" name="fechaNacimiento" id="fechaNacimiento">
+				</div>
+				<div class="form-group">
+					<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#flipFlop">Tarjeta</button>
+					<!-- The modal -->
+					<div class="modal fade" id="flipFlop" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h4 class="modal-title" id="modalLabel">
+										Datos de tarjeta
+									</h4>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<form action="#" class="credit-card-div">
+										<div class="panel panel-default" >
+										 	<div class="panel-heading"> 
+										      	<div class="row ">
+										            <div class="col-md-12">
+										                <input type="text" class="form-control" placeholder="Ingrese el numero de la tarjeta" />
+										            </div>
+										        </div>
+										     	<div class="row ">
+										            <div class="col-md-3 col-sm-3 col-xs-3">
+										                <span class="help-block text-muted small-font" > Mes</span>
+										                <input type="text" class="form-control" placeholder="MM" />
+										            </div>
+										         	<div class="col-md-3 col-sm-3 col-xs-3">
+										                <span class="help-block text-muted small-font" > Año</span>
+										                <input type="text" class="form-control" placeholder="YY" />
+										            </div>
+										        	<div class="col-md-3 col-sm-3 col-xs-3">
+										                <span class="help-block text-muted small-font" >  CCV</span>
+										                <input type="text" class="form-control" placeholder="CCV" />
+										            </div>
+										         	<div class="col-md-3 col-sm-3 col-xs-3">
+														<img src="images/card.png" style="margin-top: 20px" height="50" width="50">
+										         	</div>
+										     	</div>
+										     	<br>
+										    	<div class="row">
+										    		<div class="col-md-12 pad-adjust">
+									            		<input type="text" class="form-control" placeholder="Nombre en la tarjeta" />
+										        	</div>
+										    	</div> 
+											</div>
+										</div>
+									</form>
+								</div>
+								<div class="modal-footer">
+									<div class="row ">
+									    <div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+									        <input type="submit"  class="btn btn-danger" value="Cancelar" />
+									    </div>
+									    <div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+									        <input type="submit"  class="btn btn-warning btn-block" value="Guradar" />
+									    </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<button type="button" class="btn btn-danger btn-lg" data-toggle="" data-target="">Cancelar</button>
+					<button type="button" class="btn btn-success btn-lg" data-toggle="" data-target="">Registrar</button>
+				</div>
+			</form>
+
+		</div>
+		<div class="col-md-3">
+			<!--Futura Publicidad-->
+		</div>
+	</div>
+</div>
+
+@endsection
